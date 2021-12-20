1ce0a2822f9bc0f3d60ce89d302629cca93f308e
FroZo <frozo02@yandex.ru>
2021-12-20 17:21:41 +0800

    add socket path

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,13 +6,16 @@ import "../styles/globals.scss";
 import "react-circular-progressbar/dist/styles.css";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const SOCKET_PATH = process.env.NEXT_PUBLIC_SOCKET_PATH;
 
 function MyApp( { Component, pageProps } ){
   const [ socket, setSocket ] = useState();
   const [ totalUsers, setTotalUsers ] = useState( 0 );
 
   useEffect( () => {
-    const socket_ = io( API_URL );
+    const socket_ = io( API_URL, {
+      path: `${SOCKET_PATH}/socket.io`
+    } );
 
     socket_.on( "totalUsers", setTotalUsers );
 
